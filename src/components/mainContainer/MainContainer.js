258694a84602eb7125ab002531d748a4cab46fc2
Dan Fellner <dfellner92@gmail.com>
2021-10-12 21:04:54 -0400

    only thing that doesn't work about exacta logic is reset

---
+++ src/components/mainContainer/MainContainer.js
@@ -0,0 +1,477 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+function MainContainer() {
+  const [finalTotal, setFinalTotal] = useState(0);
+  let firstArray = [];
+  let secondArray = [];
+
+  const handleFirst = (num) => {
+    if (firstArray.includes(num)) {
+      document.getElementById(`check-first-${num}`).classList.add("hide");
+      firstArray = firstArray.filter((number) => number !== num);
+    } else {
+      document.getElementById(`check-first-${num}`).classList.remove("hide");
+      firstArray.push(num);
+    }
+    console.log(firstArray);
+  };
+
+  const handleSecond = (num) => {
+    if (secondArray.includes(num)) {
+      document.getElementById(`check-second-${num}`).classList.add("hide");
+      secondArray = secondArray.filter((number) => number !== num);
+    } else {
+      document.getElementById(`check-second-${num}`).classList.remove("hide");
+      secondArray.push(num);
+    }
+    console.log(secondArray);
+  };
+
+  console.log(finalTotal);
+
+  const findTotal = (first, second) => {
+    let secondTest = [];
+    let uniqueValues = [];
+
+    for (const element of first) {
+      for (const element2 of second) {
+        if (!first.includes(element2)) {
+          secondTest.push(element2);
+        }
+      }
+    }
+    for (let i = 0; i < secondTest.length; i++) {
+      if (!uniqueValues.includes(secondTest[i])) {
+        uniqueValues.push(secondTest[i]);
+      }
+    }
+    console.log(uniqueValues);
+    setFinalTotal(uniqueValues.length);
+  };
+
+  return (
+    <div>
+      <div class="containing">
+        <h3 class="heading">First Place</h3>
+        <div class="firstButtons">
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(1)}>
+              1
+            </button>
+            <img
+              id="check-first-1"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(2)}>
+              2
+            </button>
+            <img
+              id="check-first-2"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(3)}>
+              3
+            </button>
+            <img
+              id="check-first-3"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(4)}>
+              4
+            </button>
+            <img
+              id="check-first-4"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(5)}>
+              5
+            </button>
+            <img
+              id="check-first-5"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(6)}>
+              6
+            </button>
+            <img
+              id="check-first-6"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(7)}>
+              7
+            </button>
+            <img
+              id="check-first-7"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(8)}>
+              8
+            </button>
+            <img
+              id="check-first-8"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(9)}>
+              9
+            </button>
+            <img
+              id="check-first-9"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(10)}>
+              10
+            </button>
+            <img
+              id="check-first-10"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(11)}>
+              11
+            </button>
+            <img
+              id="check-first-11"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(12)}>
+              12
+            </button>
+            <img
+              id="check-first-12"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(13)}>
+              13
+            </button>
+            <img
+              id="check-first-13"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(14)}>
+              14
+            </button>
+            <img
+              id="check-first-14"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(15)}>
+              15
+            </button>
+            <img
+              id="check-first-15"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(16)}>
+              16
+            </button>
+            <img
+              id="check-first-16"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(17)}>
+              17
+            </button>
+            <img
+              id="check-first-17"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleFirst(18)}>
+              18
+            </button>
+            <img
+              id="check-first-18"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="containing">
+        <h3 class="heading">Second Place</h3>
+        <div class="firstButtons">
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(1)}>
+              1
+            </button>
+            <img
+              id="check-second-1"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(2)}>
+              2
+            </button>
+            <img
+              id="check-second-2"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(3)}>
+              3
+            </button>
+            <img
+              id="check-second-3"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(4)}>
+              4
+            </button>
+            <img
+              id="check-second-4"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(5)}>
+              5
+            </button>
+            <img
+              id="check-second-5"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(6)}>
+              6
+            </button>
+            <img
+              id="check-second-6"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(7)}>
+              7
+            </button>
+            <img
+              id="check-second-7"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(8)}>
+              8
+            </button>
+            <img
+              id="check-second-8"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(9)}>
+              9
+            </button>
+            <img
+              id="check-second-9"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(10)}>
+              10
+            </button>
+            <img
+              id="check-second-10"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(11)}>
+              11
+            </button>
+            <img
+              id="check-second-11"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(12)}>
+              12
+            </button>
+            <img
+              id="check-second-12"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(13)}>
+              13
+            </button>
+            <img
+              id="check-second-13"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(14)}>
+              14
+            </button>
+            <img
+              id="check-second-14"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(15)}>
+              15
+            </button>
+            <img
+              id="check-second-15"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button class="button-tag" onClick={() => handleSecond(16)}>
+              16
+            </button>
+            <img
+              id="check-second-16"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button" onClick={() => handleSecond(17)}>
+            <button class="button-tag">17</button>
+            <img
+              id="check-second-17"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+          <div class="button">
+            <button
+              value="eighteen"
+              class="button-tag"
+              onClick={() => handleSecond(18)}
+            >
+              18
+            </button>
+            <img
+              id="check-second-18"
+              class="button-tag__check hide"
+              src="https://img.icons8.com/material-outlined/24/000000/checkmark--v1.png"
+              alt="check first one"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="containing">
+        <h3 class="heading">Total</h3>
+        <div>
+          <button onClick={() => findTotal(firstArray, secondArray)}>
+            findTotal
+          </button>
+        </div>
+        <br />
+        <div>{`$${finalTotal}.00`}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MainContainer;
