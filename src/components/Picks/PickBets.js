7a4bc0cc227be020c05c0b2fd6d065b123eb402a
Dan Fellner <dfellner92@gmail.com>
2021-11-03 18:30:30 -0400

    pick 4 2-5 and refactored handle calc

--- src/components/Picks/PickBets.js
+++ src/components/Picks/PickBets.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import BELMONT_PARK_OCT_29_2021 from "../../util/belmont-park-20211029";
 import KEENELAND_10_29_21 from "../../util/keeneland-20211029";
-
 import PickLabels from "./PickLabels";
 import FirstRow from "./PickRows/FirstRow";
 import SecondRow from "./PickRows/SecondRow";
@@ -62,10 +61,8 @@ const PickBets = () => {
   };
 
   const handlePicks = (pairID, value) => {
-    console.log(pairID.slice(0, 3));
-    if (pickType === 5 || pickType === 3) {
+    if (pickType === 5 || pickType === 3 || pickType === 4) {
       if (allPickIDs.includes(pairID)) {
-        console.log("includes");
         setAllPickIDs(allPickIDs.filter((pair) => pair !== pairID));
         document.getElementById(pairID).style.backgroundColor = "";
         if (pairID.slice(0, 1) === "1") {
@@ -90,7 +87,6 @@ const PickBets = () => {
           setTenthVals(tenthVals.filter((val) => val !== value));
         }
       } else {
-        console.log("doesnt include");
         setAllPickIDs((state) => [...state, pairID]);
         document.getElementById(pairID).style.backgroundColor =
           "rgb(255, 80, 80)";
@@ -120,105 +116,6 @@ const PickBets = () => {
     }
   };
 
-  const handleCalculation = () => {
-    if (pickType === 3) {
-      if (
-        firstVals.length > 0 &&
-        secondVals.length > 0 &&
-        thirdVals.length > 0
-      ) {
-        setCalcValue(firstVals.length * secondVals.length * thirdVals.length);
-      } else if (
-        secondVals.length > 0 &&
-        thirdVals.length > 0 &&
-        fourthVals.length > 0
-      ) {
-        setCalcValue(secondVals.length * thirdVals.length * fourthVals.length);
-      } else if (
-        thirdVals.length > 0 &&
-        fourthVals.length > 0 &&
-        fifthVals.length > 0
-      ) {
-        setCalcValue(thirdVals.length * fourthVals.length * fifthVals.length);
-      } else if (
-        fourthVals.length > 0 &&
-        fifthVals.length > 0 &&
-        sixthVals.length > 0
-      ) {
-        setCalcValue(fourthVals.length * fifthVals.length * sixthVals.length);
-      } else if (
-        fifthVals.length > 0 &&
-        sixthVals.length > 0 &&
-        seventhVals.length > 0
-      ) {
-        setCalcValue(fifthVals.length * sixthVals.length * seventhVals.length);
-      } else if (
-        sixthVals.length > 0 &&
-        seventhVals.length > 0 &&
-        eighthVals.length > 0
-      ) {
-        setCalcValue(sixthVals.length * seventhVals.length * eighthVals.length);
-      } else if (
-        seventhVals.length > 0 &&
-        eighthVals.length > 0 &&
-        ninthVals.length > 0
-      ) {
-        setCalcValue(seventhVals.length * eighthVals.length * ninthVals.length);
-      } else if (
-        eighthVals.length > 0 &&
-        ninthVals.length > 0 &&
-        tenthVals.length > 0
-      ) {
-        setCalcValue(eighthVals.length * ninthVals.length * tenthVals.length);
-      }
-    } else if (pickType === 5) {
-      if (
-        firstVals.length > 0 &&
-        secondVals.length > 0 &&
-        thirdVals.length > 0 &&
-        fourthVals.length > 0 &&
-        fifthVals.length > 0 &&
-        sixthVals.length > 0 &&
-        seventhVals.length > 0 &&
-        eighthVals.length > 0 &&
-        ninthVals.length > 0 &&
-        tenthVals.length > 0
-      ) {
-        alert("One bet at a time!");
-      } else if (
-        firstVals.length > 0 &&
-        secondVals.length > 0 &&
-        thirdVals.length > 0 &&
-        fourthVals.length > 0 &&
-        fifthVals.length > 0
-      ) {
-        setCalcValue(
-          firstVals.length *
-            secondVals.length *
-            thirdVals.length *
-            fourthVals.length *
-            fifthVals.length
-        );
-      } else if (
-        sixthVals.length > 0 &&
-        seventhVals.length > 0 &&
-        eighthVals.length > 0 &&
-        ninthVals.length > 0 &&
-        tenthVals.length > 0
-      ) {
-        setCalcValue(
-          sixthVals.length *
-            seventhVals.length *
-            eighthVals.length *
-            ninthVals.length *
-            tenthVals.length
-        );
-      } else {
-        alert("Not Enough Races Selected for a Pick 5!");
-      }
-    }
-  };
-
   return (
     <div>
       <div>
@@ -230,7 +127,18 @@ const PickBets = () => {
         />
         <PickTypeMenu handlePickType={handlePickType} pickType={pickType} />
         <CostRender
-          handleCalculation={handleCalculation}
+          pickType={pickType}
+          firstVals={firstVals}
+          secondVals={secondVals}
+          thirdVals={thirdVals}
+          fourthVals={fourthVals}
+          fifthVals={fifthVals}
+          sixthVals={sixthVals}
+          seventhVals={seventhVals}
+          eighthVals={eighthVals}
+          ninthVals={ninthVals}
+          tenthVals={tenthVals}
+          setCalcValue={setCalcValue}
           calcValue={calcValue}
         />
       </div>
--- src/components/Picks/PickMenus/CostRender.js
+++ src/components/Picks/PickMenus/CostRender.js
@@ -1,9 +1,154 @@
 function CostRender(props) {
+  const handleCalculation = () => {
+    if (props.pickType === 3) {
+      if (
+        props.firstVals.length > 0 &&
+        props.secondVals.length > 0 &&
+        props.thirdVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.firstVals.length *
+            props.secondVals.length *
+            props.thirdVals.length
+        );
+      } else if (
+        props.secondVals.length > 0 &&
+        props.thirdVals.length > 0 &&
+        props.fourthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.secondVals.length *
+            props.thirdVals.length *
+            props.fourthVals.length
+        );
+      } else if (
+        props.thirdVals.length > 0 &&
+        props.fourthVals.length > 0 &&
+        props.fifthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.thirdVals.length *
+            props.fourthVals.length *
+            props.fifthVals.length
+        );
+      } else if (
+        props.fourthVals.length > 0 &&
+        props.fifthVals.length > 0 &&
+        props.sixthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.fourthVals.length *
+            props.fifthVals.length *
+            props.sixthVals.length
+        );
+      } else if (
+        props.fifthVals.length > 0 &&
+        props.sixthVals.length > 0 &&
+        props.seventhVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.fifthVals.length *
+            props.sixthVals.length *
+            props.seventhVals.length
+        );
+      } else if (
+        props.sixthVals.length > 0 &&
+        props.seventhVals.length > 0 &&
+        props.eighthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.sixthVals.length *
+            props.seventhVals.length *
+            props.eighthVals.length
+        );
+      } else if (
+        props.seventhVals.length > 0 &&
+        props.eighthVals.length > 0 &&
+        props.ninthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.seventhVals.length *
+            props.eighthVals.length *
+            props.ninthVals.length
+        );
+      } else if (
+        props.eighthVals.length > 0 &&
+        props.ninthVals.length > 0 &&
+        props.tenthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.eighthVals.length *
+            props.ninthVals.length *
+            props.tenthVals.length
+        );
+      }
+    } else if (props.pickType === 4) {
+      if (
+        props.secondVals.length > 0 &&
+        props.thirdVals.length > 0 &&
+        props.fourthVals.length > 0 &&
+        props.fifthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.secondVals.length *
+            props.thirdVals.length *
+            props.fourthVals.length *
+            props.fifthVals.length
+        );
+      }
+    } else if (props.pickType === 5) {
+      if (
+        props.firstVals.length > 0 &&
+        props.secondVals.length > 0 &&
+        props.thirdVals.length > 0 &&
+        props.fourthVals.length > 0 &&
+        props.fifthVals.length > 0 &&
+        props.sixthVals.length > 0 &&
+        props.seventhVals.length > 0 &&
+        props.eighthVals.length > 0 &&
+        props.ninthVals.length > 0 &&
+        props.tenthVals.length > 0
+      ) {
+        alert("One bet at a time!");
+      } else if (
+        props.firstVals.length > 0 &&
+        props.secondVals.length > 0 &&
+        props.thirdVals.length > 0 &&
+        props.fourthVals.length > 0 &&
+        props.fifthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.firstVals.length *
+            props.secondVals.length *
+            props.thirdVals.length *
+            props.fourthVals.length *
+            props.fifthVals.length
+        );
+      } else if (
+        props.sixthVals.length > 0 &&
+        props.seventhVals.length > 0 &&
+        props.eighthVals.length > 0 &&
+        props.ninthVals.length > 0 &&
+        props.tenthVals.length > 0
+      ) {
+        props.setCalcValue(
+          props.sixthVals.length *
+            props.seventhVals.length *
+            props.eighthVals.length *
+            props.ninthVals.length *
+            props.tenthVals.length
+        );
+      } else {
+        alert("Not Enough Races Selected for a Pick 5!");
+      }
+    }
+  };
+
   return (
     <div className="cost-block">
       <button
         className="cost-block__button"
-        onClick={() => props.handleCalculation()}
+        onClick={() => handleCalculation()}
       >
         Calculate Wage Cost
       </button>
